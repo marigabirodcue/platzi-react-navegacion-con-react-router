af918991b6eda4d1faf74ea93d7c84fe6dc11c86
marigabirodcue <rodriguezcmariaga@gmail.com>
2023-07-11 00:01:00 -0400

    Definiendo rutas con el HashRouter /#/ en TODO Machine app

---
+++ src/BlogPage.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function BlogPage() {
+    return (
+        <h1>BlogPage</h1>
+    );
+}
+
+export { BlogPage };
---
+++ src/ProfilePage.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function ProfilePage() {
+    return (
+        <h1>ProfilePage</h1>
+    );
+}
+
+export { ProfilePage };
