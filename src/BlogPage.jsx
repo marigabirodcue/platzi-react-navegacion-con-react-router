5ee62ae45e6bc915d7025e5c792bca771047cdec
marigabirodcue <rodriguezcmariaga@gmail.com>
2023-07-11 23:04:37 -0400

    Definiendo rutas dinámicas en BlogPost y con Push en TODO Machine app

--- src/BlogPage.jsx
+++ src/BlogPage.jsx
@@ -1,9 +1,26 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { blogdata } from './blogdata'
 
 function BlogPage() {
     return (
-        <h1>BlogPage</h1>
+        <>
+            <h1>BlogPage</h1>
+
+            <ul>
+                {blogdata.map(post => (
+                    <BlogLink post={post} />
+                ))}
+            </ul>
+        </>
+    );
+}
+
+function BlogLink({ post }) {
+    return (
+        <li>
+            <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+        </li>
     );
 }
 
